db896c9602259695241f565f39d15e6f643ccdca
Oguzhanolgun <oguzhanolgun35.5@gmail.com>
2021-02-03 14:39:11 +0000

    Homework Done

--- odev.js
+++ odev.js
@@ -14,7 +14,20 @@ var car = {
 }
 
 var myCarDetails =  car.displayDetails;
-myCarDetails();
+// 1.Çözüm >>> Bind ile fonksiyonu nesneye bağlamak
+   var myCarDetails =  car.displayDetails;
+   var myToyoto = myCarDetails.bind(car);
+   myToyoto();
+
+   // 2. Çözüm >>> call ya da apply kulanmak 
+    
+   myCarDetails.call(car); 
+   myCarDetails.apply(car);
+
+   /* 
+   ancak apply bu örnekte çalışmasına rağmen normalde fonksiyona parametre olarak argüman listesi gönderir call da fonksiyona tek tek argüman gönderir:
+   
+   */
 
 
 /** 
@@ -29,8 +42,26 @@ bosluk icerebilir, ancak bosluk haridcindeki isimler en az 2 karakterden olusmal
 
 function isValidName(name) {
   /// your code here
-}
+  let init;
+    if(typeof(name) === 'string' && name !== null){
+        let removeOuterSpace = name.trim();
+        let rename = removeOuterSpace.split(/[\s,]+/); 
+        rename.some(e => e.length < 2 ) ? init = false : init = true; 
+    }else {
+        
+         return init = false;
 
+          }
+
+   return init;
+}
+console.log(isValidName("Ahmet") === true); // true
+ console.log(isValidName(false) === false); // true
+ console.log(isValidName(null) === false);  // true
+ console.log(isValidName(undefined) === false); // true
+ console.log(isValidName("") === false);        // true
+ console.log(isValidName("  \t\n") === false);  // true
+ console.log(isValidName("X") === false);       // true
 
 
 /**
@@ -50,6 +81,14 @@ function summary(genre, year) {
     `${this.title} was written by ${this.author}. It is a ${genre} novel written in ${year}.`,
   )
 }
+
+// 1.çözüm
+summary.call(book, "dystopian", 1932);
+// 2.çözüm
+summary.apply(book, ["dystopian", 1932]);
+// 3.çözüm
+let summaryBook = summary.bind(book, "dystopian", 1932);
+summaryBook();
 
 
 
